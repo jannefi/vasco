f5d66ed51675bf4711ad891d03a8f83f21c0c6d7
Janne Ahlberg <janne.ahlberg@insta.fi>
2026-02-10 15:07:02 +0200

    column name update

--- scripts/fetch_supercosmos_stilts.sh
+++ scripts/fetch_supercosmos_stilts.sh
@@ -16,14 +16,14 @@ OUTPARQ="$OUTROOT/flags_supercosmos.parquet"
 stilts tcopy in="$POS" ifmt=votable out="$CSV" ofmt=csv
 stilts tcopy in="$CSV" ifmt=csv     out="$VOT" ofmt=votable
 
-ADQL="SELECT u.NUMBER, COUNT(*) AS nmatch
+ADQL="SELECT u.row_id, COUNT(*) AS nmatch
 FROM TAP_UPLOAD.t1 AS u
 JOIN supercosmos.sources AS s
   ON 1 = CONTAINS(
        POINT('ICRS', s.raj2000, s.dej2000),
        CIRCLE('ICRS', u.ra, u.dec, 5.0/3600.0)
      )
-GROUP BY u.NUMBER"
+GROUP BY u.row_id"
 
 stilts tapquery \
   tapurl=https://dc.g-vo.org/__system__/tap/run \
@@ -35,11 +35,11 @@ python - <<'PY' "$OUTCSV" "$OUTPARQ"
 import sys, pandas as pd, pyarrow as pa, pyarrow.parquet as pq
 csv, outp = sys.argv[1], sys.argv[2]
 df = pd.read_csv(csv)
-if not df.empty and 'NUMBER' in df.columns:
-    df['NUMBER'] = df['NUMBER'].astype(str)
-    flag = df[['NUMBER']].drop_duplicates().assign(is_supercosmos_artifact=True)
+if not df.empty and 'row_id' in df.columns:
+    df['row_id'] = df['row_id'].astype(str)
+    flag = df[['row_id']].drop_duplicates().assign(is_supercosmos_artifact=True)
 else:
-    flag = pd.DataFrame(columns=['NUMBER','is_supercosmos_artifact'])
+    flag = pd.DataFrame(columns=['row_id','is_supercosmos_artifact'])
 pq.write_table(pa.Table.from_pandas(flag, preserve_index=False), outp)
 print('[OK] SuperCOSMOS flags ->', outp, 'rows=', len(flag))
 PY
